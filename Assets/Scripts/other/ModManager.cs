94104f343c65a1b0b00a65928edb7cde5b52e767
NC <keyetpro@gmail.com>
2023-08-27 13:44:50 +0300

    sba load skins templ

--- Assets/Scripts/other/ModManager.cs
+++ Assets/Scripts/other/ModManager.cs
@@ -1,8 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using System.IO;
+using UnityEngine.Networking;
 
 public class ModManager
 {
-    
+    public static string skinName = "defaultskin";
+
+    public string SkinPathByName(string skinName)
+    {
+        string url = Path.Combine(Application.streamingAssetsPath, "Images/Skins/"+skinName+".png");
+        if (File.Exists(url))
+        {
+            return url;
+        }
+        url = Path.Combine(Application.streamingAssetsPath, "Images/Skins/"+"defaultskin.png");
+        return url;
+    }
 }
